2a2cfc317d0125a0dc9637fb0abdc1ccac52f2e2
maulikk04 <118893402+maulikk04@users.noreply.github.com>
2023-10-30 15:43:22 +0530

    OAuth with passport-js

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+    google : {
+        clientID:'277471713955-hl8o5cm6p2tj0acbjc19gvbi91d0u8bt.apps.googleusercontent.com',
+        clientSecret : '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'
+    },
+
+    mongodb:{
+        dbUri: 'mongodb+srv://maulikkansara04:fCAzbj5WVMnzVi47@cluster0.tqgo3l4.mongodb.net/Cluster0?retryWrites=true&w=majority'
+    },
+
+    session:{
+        cookiekey: 'abcdefghijk'
+    }
+}
